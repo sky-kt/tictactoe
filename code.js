c82f3c90ee29200a2e514e92fff1464235876bf5
sky-kt <skyler.tabo@gmail.com>
2021-06-13 15:09:19 -0700

    add brute force win detection

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ document.querySelectorAll('.section').forEach(item => {
     item.addEventListener('click', event => {
         let symbol = boardManager.setSymbol(item.id.replace('sec', ''))
         updateBoard(item.id, symbol)
+        checkIfWinner()
         console.log(gameBoard.board)
     })
 })
@@ -13,6 +14,59 @@ let updateBoard = (section, symbol) => {
     console.log(symbol)
     sectionToChange.textContent = symbol 
 
+}
+
+let checkIfWinner = () => {
+    let winner = ""
+    let board = gameBoard.board
+
+    //check horizontal rows
+    if(board[0] === board[1] && board[1] === board[2]) {
+        if(board[0] === 'X') {
+            winner = 'X'
+        } else if(board[0] === 'O') winner = 'O'
+    }
+    else if(board[3] === board[4] && board[4] === board[5]) {
+        if(board[3] === 'X') {
+            winner = 'X'
+        } else if(board[3] === 'X') winner = 'O'
+    }
+    else if(board[6] === board[7] && board[7] === board[8]) {
+        if(board[6] === 'X') {
+            winner = 'X'
+        } else if(board[6] === 'X') winner = 'O'
+    }
+
+    //check vertical rows
+    if(board[0] === board[3] && board[3] === board[6]) {
+        if(board[0] === 'X') {
+            winner = 'X'
+        } else if(board[0] === 'O') winner = 'O'
+    }
+    else if(board[1] === board[4] && board[4] === board[7]) {
+        if(board[1] === 'x') {
+            winner = 'X'
+        } else if(board[1] === 'O') winner = 'O'
+    }
+    else if(board[2] === board[5] && board[5] === board[8]) {
+        if(board[2] === 'x') {
+            winner = 'X'
+        } else if(board[2] === 'O') winner = 'O'
+    }
+
+    //check diagonals
+    if(board[0] === board[4] && board[4] === board[8]) {
+        if(board[0] === 'X') {
+            winner = 'X'
+        } else if(board[0] === 'O') winner = 'O'
+    }
+
+    if(board[2] === board[4] && board[4] === board[6]) {
+        if(board[2] === 'X') {
+            winner = 'X'
+        } else if(board[2] === 'O') winner = 'O'
+    }
+    if (winner !== "") alert(`${winner} wins!`)
 }
 
 let boardManager = (() => {
