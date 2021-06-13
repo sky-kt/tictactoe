63dc6e4a56ac01241665921b3e5252933f037845
sky-kt <skyler.tabo@gmail.com>
2021-06-13 14:28:38 -0700

    clicking draws symbol on screen

--- code.js
+++ code.js
@@ -2,27 +2,30 @@ let BOARD = document.getElementById('board')
 
 document.querySelectorAll('.section').forEach(item => {
     item.addEventListener('click', event => {
-        boardManager.setSymbol(item.id.replace('sec', ''))
-        updateBoard()
+        let symbol = boardManager.setSymbol(item.id.replace('sec', ''))
+        updateBoard(item.id, symbol)
         console.log(gameBoard.board)
     })
 })
 
-let updateBoard = () => {
-    console.log("updateBoard runs")
-    for (let item in gameBoard.board) {
-        if (gameBoard.board[item] !== '__') {
-            boardChildren.childNodes[item].text = gameBoard.board[item]
-        }
-    }
-    console.log('finished')
+let updateBoard = (section, symbol) => {
+    let sectionToChange = document.getElementById(section)
+    console.log(symbol)
+    sectionToChange.textContent = symbol 
+
 }
 
 let boardManager = (() => {
     let counter = 1
     let setSymbol = (tileIndex) => {
-        if (counter++ % 2 != 0) gameBoard.board[tileIndex] = 'X' 
-        else gameBoard.board[tileIndex] = 'O'
+        if (counter++ % 2 != 0) {
+            gameBoard.board[tileIndex] = 'X'
+            return 'X'
+        }
+        else {
+            gameBoard.board[tileIndex] = 'O'
+            return 'O'
+        }
     }
     return { counter, setSymbol }
 })()
