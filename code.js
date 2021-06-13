0e04e82b1548e37eba5155072192d40bce514ffa
sky-kt <skyler.tabo@gmail.com>
2021-06-13 15:54:50 -0700

    formatting change

--- code.js
+++ code.js
@@ -11,49 +11,41 @@ let checkIfWinner = () => {
 
     //check horizontal rows
     if(board[0] === board[1] && board[1] === board[2]) {
-        if(board[0] === 'X') {
-            winner = 'X'
-        } else if(board[0] === 'O') winner = 'O'
+        if(board[0] === 'X') { winner = 'X'} 
+        else if(board[0] === 'O') winner = 'O'
     }
     else if(board[3] === board[4] && board[4] === board[5]) {
-        if(board[3] === 'X') {
-            winner = 'X'
-        } else if(board[3] === 'X') winner = 'O'
+        if(board[3] === 'X') { winner = 'X'}
+        else if(board[3] === 'X') winner = 'O'
     }
     else if(board[6] === board[7] && board[7] === board[8]) {
-        if(board[6] === 'X') {
-            winner = 'X'
-        } else if(board[6] === 'X') winner = 'O'
+        if(board[6] === 'X') winner = 'X'
+        else if(board[6] === 'X') winner = 'O'
     }
 
     //check vertical rows
     if(board[0] === board[3] && board[3] === board[6]) {
-        if(board[0] === 'X') {
-            winner = 'X'
-        } else if(board[0] === 'O') winner = 'O'
+        if(board[0] === 'X') winner = 'X'
+        else if(board[0] === 'O') winner = 'O'
     }
     else if(board[1] === board[4] && board[4] === board[7]) {
-        if(board[1] === 'X') {
-            winner = 'X'
-        } else if(board[1] === 'O') winner = 'O'
+        if(board[1] === 'X') winner = 'X'
+        else if(board[1] === 'O') winner = 'O'
     }
     else if(board[2] === board[5] && board[5] === board[8]) {
-        if(board[2] === 'X') {
-            winner = 'X'
-        } else if(board[2] === 'O') winner = 'O'
+        if(board[2] === 'X') winner = 'X'
+        else if(board[2] === 'O') winner = 'O'
     }
 
     //check diagonals
     if(board[0] === board[4] && board[4] === board[8]) {
-        if(board[0] === 'X') {
-            winner = 'X'
-        } else if(board[0] === 'O') winner = 'O'
+        if(board[0] === 'X') winner = 'X'
+        else if(board[0] === 'O') winner = 'O'
     }
 
     if(board[2] === board[4] && board[4] === board[6]) {
-        if(board[2] === 'X') {
-            winner = 'X'
-        } else if(board[2] === 'O') winner = 'O'
+        if(board[2] === 'X') winner = 'X'
+        else if(board[2] === 'O') winner = 'O'
     }
 
     //state winner
